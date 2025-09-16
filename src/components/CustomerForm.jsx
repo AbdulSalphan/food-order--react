54ecf39d65fd39b6e1a5b7986c8a15cf5474df94
Abdul Salphan <abdulsalphan@gmail.com>
2025-09-17 00:10:23 +0530

    Success card.

--- src/components/CustomerForm.jsx
+++ src/components/CustomerForm.jsx
@@ -1,10 +1,11 @@
-import { useActionState, useContext } from 'react';
+import { useActionState, useContext, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import Submit from './Submit';
 import { AppContext } from './store/app-context';
 
 export default function CustomerForm({cartValue, ref}) {
     const {cartItems} = useContext(AppContext);
+    const [formSubmitted, setFormSubmitted] = useState();
 
     async function formSubmitHandler(prevData, formData) {
         const name = formData.get('name');
@@ -59,7 +60,6 @@ export default function CustomerForm({cartValue, ref}) {
     async function postData(data) {
         let cleanData = compileData(data)
         console.log(JSON.stringify(cleanData));
-        // console.log('[{"items":[{"id":"m1","name":"Mac & Cheese","price":"8.99","description":"Creamy cheddar cheese mixed with perfectly cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.","image":"images/mac-and-cheese.jpg","quantity":1},{"id":"m2","name":"Margherita Pizza","price":"12.99","description":"A classic pizza with fresh mozzarella, tomatoes, and basil on a thin and crispy crust.","image":"images/margherita-pizza.jpg","quantity":1}],"customer":{"name":"Abdul Salphan","email":"abdulsalphan@gmail.com","street":"457/1, PILAIYAR KOIL STREET","postal-code":"603003","city":"Chengalpattu"},"id":"793.0587786106436"},{"items":[{"id":"m1","name":"Mac & Cheese","price":"8.99","description":"Creamy cheddar cheese mixed with perfectly cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.","image":"images/mac-and-cheese.jpg","quantity":1}],"customer":{"name":"Abdul Salphan","email":"abdulsalphan@gmail.com","street":"457/1, PILAIYAR KOIL STREET","postal-code":"603003","city":"Chengalpattu"},"id":"940.4809135886399"}]')
         try {
             const response = await fetch('http://localhost:3000/orders', {
                 method: 'POST',
@@ -68,6 +68,7 @@ export default function CustomerForm({cartValue, ref}) {
                 },
                 body: JSON.stringify(cleanData)
             });
+            setFormSubmitted(response);
         } catch (error) {
             console.log(error);
         }
@@ -83,47 +84,68 @@ export default function CustomerForm({cartValue, ref}) {
         }
     }
 
+    function modalClose() {
+        ref.current.close();
+    }
+
+    let successContent = false;
+
+    if(formSubmitted) {
+        successContent = (    
+            <>
+                <h2>Success!</h2>
+                <p>Your order has been placed successfully!</p>
+                <p>Check your email inbox for the receipt.</p>
+                <p className="modal-actions">
+                    <button className="button" onClick={modalClose} type='button'>Okay</button>
+                </p>
+            </>
+        )
+    }
+
     const [formState, formAction] = useActionState(formSubmitHandler, {errors: null});
 
     return createPortal(
         <dialog className="modal" ref={ref}>
-            <form action={formAction}>
-                <h2>Checkout</h2>
-                <p>Total Amount: ${cartValue}</p>
-                <p className="control">
-                    <label htmlFor="name">Full Name</label>
-                    <input id="name" type="text" name="name" defaultValue={formState.name?.value}  />
-                </p>
-                <p className="control">
-                    <label htmlFor="email">E-Mail Address</label>
-                    <input id="email" type="text" name="email" defaultValue={formState.email?.value}  />
-                </p>
-                <p className="control">
-                    <label htmlFor="street">Street</label>
-                    <input id="street" type="text" name="street" defaultValue={formState.street?.value}  />
-                </p>
-                <div className="control-row">
+            {successContent ? successContent :
+                <form action={formAction}>
+                    <h2>Checkout</h2>
+                    <p>Total Amount: ${cartValue}</p>
                     <p className="control">
-                        <label htmlFor="postal-code">Postal Code</label>
-                        <input id="postal-code" type="text" name="postal-code" defaultValue={formState.postalCode?.value}  />
+                        <label htmlFor="name">Full Name</label>
+                        <input id="name" type="text" name="name" defaultValue={formState.name?.value}  />
                     </p>
                     <p className="control">
-                        <label htmlFor="city">City</label>
-                        <input id="city" type="text" name="city" defaultValue={formState.city?.value}  />
+                        <label htmlFor="email">E-Mail Address</label>
+                        <input id="email" type="text" name="email" defaultValue={formState.email?.value}  />
                     </p>
-                </div>
-                {formState.errors &&
-                <ul className="error">
-                    {formState.errors.map(error => {
-                        return <li key={error}>{error}</li>
-                    })}
-                </ul>
-                }
-                <p className="modal-actions">
-                    <button className='text-button' type='button'>Close</button>
-                    <Submit />
-                </p>
-            </form>
+                    <p className="control">
+                        <label htmlFor="street">Street</label>
+                        <input id="street" type="text" name="street" defaultValue={formState.street?.value}  />
+                    </p>
+                    <div className="control-row">
+                        <p className="control">
+                            <label htmlFor="postal-code">Postal Code</label>
+                            <input id="postal-code" type="text" name="postal-code" defaultValue={formState.postalCode?.value}  />
+                        </p>
+                        <p className="control">
+                            <label htmlFor="city">City</label>
+                            <input id="city" type="text" name="city" defaultValue={formState.city?.value}  />
+                        </p>
+                    </div>
+                    {formState.errors &&
+                    <ul className="error">
+                        {formState.errors.map(error => {
+                            return <li key={error}>{error}</li>
+                        })}
+                    </ul>
+                    }
+                    <p className="modal-actions">
+                        <button className='text-button' onClick={modalClose} type='button'>Close</button>
+                        <Submit />
+                    </p>
+                </form>
+            }
         </dialog>,
         document.getElementById('modal')
     )
